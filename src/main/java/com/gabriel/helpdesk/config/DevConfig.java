37966523d22de8d92b0b43dc95915455503ea09d
GabrielSanga <gabrie.sanga.fef@gmail.com>
2021-09-11 20:49:55 -0300

    Criado Perfil de Desenvolvimento

--- src/main/java/com/gabriel/helpdesk/config/DevConfig.java
+++ src/main/java/com/gabriel/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.gabriel.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,15 +9,21 @@ import org.springframework.context.annotation.Profile;
 import com.gabriel.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
-	public void instanciaDB() {
-		this.dbService.instanciaDB();
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
 	}
 
 }
