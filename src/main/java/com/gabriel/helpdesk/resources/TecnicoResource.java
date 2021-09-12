68e7e8d8a47c4a40f9e987beeaca239e5d47d215
GabrielSanga <gabrie.sanga.fef@gmail.com>
2021-09-11 22:15:34 -0300

    Criado EndPoint FindById para Tecnico]

---
+++ src/main/java/com/gabriel/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,28 @@
+package com.gabriel.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabriel.helpdesk.domain.Tecnico;
+import com.gabriel.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+		
+		Tecnico oTecnico = tecnicoService.findById(id);
+		
+		return ResponseEntity.ok().body(oTecnico);	
+	}
+
+}
