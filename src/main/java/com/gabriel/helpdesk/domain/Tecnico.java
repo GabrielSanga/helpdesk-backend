f2ebaad90b013a84015de265d983357ed2993cf0
GabrielSanga <gabrie.sanga.fef@gmail.com>
2021-09-11 22:12:52 -0300

    Corrigido situações de serializable

--- src/main/java/com/gabriel/helpdesk/domain/Tecnico.java
+++ src/main/java/com/gabriel/helpdesk/domain/Tecnico.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gabriel.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -13,6 +14,7 @@ public class Tecnico extends Pessoa{
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
